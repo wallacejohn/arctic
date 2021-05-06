0b0d5817cf45d5253664af928caa21c83757d089
John Wallace <wallacejohn@protonmail.com>
2021-05-05 21:19:09 -0500

    Update pandas version and remove Panel dependency

--- arctic/__init__.py
+++ arctic/__init__.py
@@ -3,13 +3,18 @@
 from .arctic import Arctic, register_library_type
 from .arctic import VERSION_STORE, TICK_STORE, CHUNK_STORE
 from .store._ndarray_store import NdarrayStore
-from .store._pandas_ndarray_store import PandasDataFrameStore, PandasSeriesStore, PandasPanelStore
+from .store._pandas_ndarray_store import (
+    PandasDataFrameStore,
+    PandasSeriesStore,
+    PandasPanelStore,
+)
 from .store.version_store import register_versioned_storage, register_version
 
 try:
     from pkg_resources import get_distribution
+
     str_version = get_distribution(__name__).version.strip()
-    int_parts = tuple(int(x) for x in str_version.split('.'))
+    int_parts = tuple(int(x) for x in str_version.split("."))
     num_version = sum([1000 ** i * v for i, v in enumerate(reversed(int_parts))])
     register_version(str_version, num_version)
 except Exception:
@@ -24,5 +29,10 @@ else:
 
 register_versioned_storage(PandasDataFrameStore)
 register_versioned_storage(PandasSeriesStore)
-register_versioned_storage(PandasPanelStore)
+try:
+    from pandas import Panel
+
+    register_versioned_storage(PandasPanelStore)
+except ImportError:
+    pass
 register_versioned_storage(NdarrayStore)
--- setup.py
+++ setup.py
@@ -23,12 +23,12 @@ from setuptools import find_packages
 from setuptools import setup
 from setuptools.command.test import test as TestCommand
 
-long_description = open('README.md').read()
-changelog = open('CHANGES.md').read()
+long_description = open("README.md").read()
+changelog = open("CHANGES.md").read()
 
 
 class PyTest(TestCommand):
-    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+    user_options = [("pytest-args=", "a", "Arguments to pass to py.test")]
 
     def initialize_options(self):
         TestCommand.initialize_options(self)
@@ -40,18 +40,31 @@ class PyTest(TestCommand):
         self.test_suite = True
 
     def run_tests(self):
-        logging.basicConfig(format='%(asctime)s %(levelname)s %(name)s %(message)s', level='DEBUG')
+        logging.basicConfig(
+            format="%(asctime)s %(levelname)s %(name)s %(message)s", level="DEBUG"
+        )
 
         # import here, cause outside the eggs aren't loaded
         import pytest
         import six
 
-        args = [self.pytest_args] if isinstance(self.pytest_args, six.string_types) else list(self.pytest_args)
-        args.extend(['--cov', 'arctic',
-                     '--cov-report', 'xml',
-                     '--cov-report', 'html',
-                     '--junitxml', 'junit.xml',
-                     ])
+        args = (
+            [self.pytest_args]
+            if isinstance(self.pytest_args, six.string_types)
+            else list(self.pytest_args)
+        )
+        args.extend(
+            [
+                "--cov",
+                "arctic",
+                "--cov-report",
+                "xml",
+                "--cov-report",
+                "html",
+                "--junitxml",
+                "junit.xml",
+            ]
+        )
         errno = pytest.main(args)
         sys.exit(errno)
 
@@ -63,48 +76,58 @@ setup(
     author_email="ManAHLTech@ahl.com",
     description=("AHL Research Versioned TimeSeries and Tick store"),
     license="GPL",
-    keywords=["ahl", "keyvalue", "tickstore", "mongo", "timeseries", ],
+    keywords=[
+        "ahl",
+        "keyvalue",
+        "tickstore",
+        "mongo",
+        "timeseries",
+    ],
     url="https://github.com/manahl/arctic",
-    packages=find_packages(exclude=['tests', 'tests.*', 'benchmarks']),
-    long_description='\n'.join((long_description, changelog)),
+    packages=find_packages(exclude=["tests", "tests.*", "benchmarks"]),
+    long_description="\n".join((long_description, changelog)),
     long_description_content_type="text/markdown",
-    cmdclass={'test': PyTest},
-    setup_requires=["six",
-                    "numpy<=1.18.4",
-                    "setuptools-git",
-                   ],
-    install_requires=["decorator",
-                      "enum-compat",
-                      "futures; python_version == '2.7'",
-                      "mockextras",
-                      "pandas<=1.0.3",
-                      "pymongo>=3.6.0",
-                      "python-dateutil",
-                      "pytz",
-                      "tzlocal",
-                      "lz4"
-                     ],
+    cmdclass={"test": PyTest},
+    setup_requires=[
+        "six",
+        "numpy<=1.18.4",
+        "setuptools-git",
+    ],
+    install_requires=[
+        "decorator",
+        "enum-compat",
+        "futures; python_version == '2.7'",
+        "mockextras",
+        "pandas>=1.2.0",
+        "pymongo>=3.6.0",
+        "python-dateutil",
+        "pytz",
+        "tzlocal",
+        "lz4",
+    ],
     # Note: pytest >= 4.1.0 is not compatible with pytest-cov < 2.6.1.
-    tests_require=["mock",
-                   "mockextras",
-                   "pytest",
-                   "pytest-cov",
-                   "pytest-server-fixtures",
-                   "pytest-timeout",
-                   "pytest-xdist<=1.26.1",
-                   "lz4"
-                  ],
-    entry_points={'console_scripts': [
-                                        'arctic_init_library = arctic.scripts.arctic_init_library:main',
-                                        'arctic_list_libraries = arctic.scripts.arctic_list_libraries:main',
-                                        'arctic_delete_library = arctic.scripts.arctic_delete_library:main',
-                                        'arctic_enable_sharding = arctic.scripts.arctic_enable_sharding:main',
-                                        'arctic_copy_data = arctic.scripts.arctic_copy_data:main',
-                                        'arctic_create_user = arctic.scripts.arctic_create_user:main',
-                                        'arctic_prune_versions = arctic.scripts.arctic_prune_versions:main',
-                                        'arctic_fsck = arctic.scripts.arctic_fsck:main',
-                                        ]
-                  },
+    tests_require=[
+        "mock",
+        "mockextras",
+        "pytest",
+        "pytest-cov",
+        "pytest-server-fixtures",
+        "pytest-timeout",
+        "pytest-xdist<=1.26.1",
+        "lz4",
+    ],
+    entry_points={
+        "console_scripts": [
+            "arctic_init_library = arctic.scripts.arctic_init_library:main",
+            "arctic_list_libraries = arctic.scripts.arctic_list_libraries:main",
+            "arctic_delete_library = arctic.scripts.arctic_delete_library:main",
+            "arctic_enable_sharding = arctic.scripts.arctic_enable_sharding:main",
+            "arctic_copy_data = arctic.scripts.arctic_copy_data:main",
+            "arctic_create_user = arctic.scripts.arctic_create_user:main",
+            "arctic_prune_versions = arctic.scripts.arctic_prune_versions:main",
+            "arctic_fsck = arctic.scripts.arctic_fsck:main",
+        ]
+    },
     classifiers=[
         "Development Status :: 4 - Beta",
         "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
